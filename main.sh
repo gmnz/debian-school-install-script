68e428f53dae54b4d46fab31de8f6ccfbd2f1091
gmnz <michael@x200t>
2018-06-01 12:49:20 +0200

    Still no empty disk support

--- main.sh
+++ main.sh
@@ -17,8 +17,8 @@ DSIZE=$(parted /dev/sda -s -- p | grep Disk | head -n 1 | awk '{ print $3 }' | a
 if [ -z $PARTN ]; then
         if [ $(($(echo $DTAB | grep sda | wc -l) - 1)) -gt 3 ]; then
           parted /dev/sda -s -- rm 4
-        fi
-        if fdisk -l /dev/sda | grep -q -e FAT -e NTFS; then
+          PEND=$(parted /dev/sda -s -- p | tail -n 2 | head -n 1 | awk '{ print $3 }' | awk -F. '{ print $1 }' | awk -F'G' ' { print $1 }')
+        elif fdisk -l /dev/sda | grep -q -e FAT -e NTFS; then
 
           PEND=$(parted /dev/sda -s -- p | tail -n 2 | head -n 1 | awk '{ print $3 }' | awk -F. '{ print $1 }' | awk -F'G' ' { print $1 }')
 
@@ -30,11 +30,12 @@ if [ -z $PARTN ]; then
             PEND=$(parted /dev/sda -s -- p | tail -n 2 | head -n 1 | awk '{ print $3 }' | awk -F. '{ print $1 }' | awk -F'G' ' { print $1 }')
           fi
 
-          parted /dev/sda -s -- mkpart extended $((PEND))"GB" 100%
-          parted /dev/sda -s -- mkpart logical ext4 $((PEND))"GB" $((PEND+10))"GB"
-          parted /dev/sda -s -- mkpart logical linux-swap $((PEND+10))"GB" 100%
-
 	fi
+
+        parted /dev/sda -s -- mkpart extended $((PEND))"GB" 100%
+        parted /dev/sda -s -- mkpart logical ext4 $((PEND))"GB" $((PEND+10))"GB"
+        parted /dev/sda -s -- mkpart logical linux-swap $((PEND+10))"GB" 100%
+
 fi
 
 SWAPP=$(echo "$DTAB" | grep "Linux swap" | awk '{ print $1 }')
