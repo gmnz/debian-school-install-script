ff40557e32f88d47777594432c11ad21491071a0
gmnz <michael@x200t>
2018-06-01 12:23:01 +0200

    Better partition state detection

--- main.sh
+++ main.sh
@@ -9,17 +9,16 @@ echo -n "root passwd: "
 read -s PASSWD
 
 DTAB=$(fdisk -l /dev/sda)
+PARTN=$(echo "$DTAB" | grep -e Linux | grep -v "Linux swap" | awk '{ print $1 }')
+DSIZE=$(parted /dev/sda -s -- p | grep Disk | head -n 1 | awk '{ print $3 }' | awk -F'G' '{ print $1 }')
 
-if fdisk -l /dev/sda | grep -q -e FAT -e NTFS; then
-	PARTN=$(echo "$DTAB" | grep -e Linux | grep -v "Linux swap" | awk '{ print $1 }')
-	if [ -z $PARTN ]; then
-
-          if [ $(($(echo $DTAB | grep sda | wc -l) - 1)) -gt 3 ]; then
-            parted /dev/sda -s -- rm 4
-          fi
+if [ -z $PARTN ]; then
+        if [ $(($(echo $DTAB | grep sda | wc -l) - 1)) -gt 3 ]; then
+          parted /dev/sda -s -- rm 4
+        fi
+        if fdisk -l /dev/sda | grep -q -e FAT -e NTFS; then
 
           PEND=$(parted /dev/sda -s -- p | tail -n 2 | head -n 1 | awk '{ print $3 }' | awk -F. '{ print $1 }' | awk -F'G' ' { print $1 }')
-          DSIZE=$(parted /dev/sda -s -- p | grep Disk | head -n 1 | awk '{ print $3 }' | awk -F'G' '{ print $1 }')
 
           if [ $PEND -eq $DSIZE ]; then
             PARTN=$(echo "$DTAB" | tail -n 1 | awk '{ print $1 }')
@@ -29,13 +28,11 @@ if fdisk -l /dev/sda | grep -q -e FAT -e NTFS; then
             PEND=$(parted /dev/sda -s -- p | tail -n 2 | head -n 1 | awk '{ print $3 }' | awk -F. '{ print $1 }' | awk -F'G' ' { print $1 }')
           fi
 
-            parted /dev/sda -s -- mkpart extended $((PEND))"GB" 100%
-            parted /dev/sda -s -- mkpart logical ext4 $((PEND))"GB" $((PEND+10))"GB"
-            parted /dev/sda -s -- mkpart logical linux-swap $((PEND+10))"GB" 100%
+          parted /dev/sda -s -- mkpart extended $((PEND))"GB" 100%
+          parted /dev/sda -s -- mkpart logical ext4 $((PEND))"GB" $((PEND+10))"GB"
+          parted /dev/sda -s -- mkpart logical linux-swap $((PEND+10))"GB" 100%
 
 	fi
-else	
-	PARTN="/dev/sda1"
 fi
 
 SWAPP=$(echo "$DTAB" | grep "Linux swap" | awk '{ print $1 }')
