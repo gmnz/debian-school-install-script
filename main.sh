e4fcf6d8a06559c18679cc021a2c07d49bf565ef
Your Name <you@example.com>
2019-07-08 08:44:34 +0000

    variables changed to lowercase; biggest disk detection

--- main.sh
+++ main.sh
@@ -3,85 +3,87 @@
 set -e
 
 echo
-echo -n "HOSTNAME: "
-read HOSTNAME
+echo -n "hostname: "
+read hostname
 echo -n "root passwd: "
-read -s PASSWD
+read -s passwd
 echo
 
 umount /dev/sda? || echo "sda unmounted"
 echo
 
+dsize=$(fdisk -l | grep "Disk /dev/sd" | awk -F":" '{ print $2 }' | awk -F"GiB" '{ print $1 }' | awk -F"." '{ print $1 }' | sort -n | tail -n 1 | tr -d ' ')
+
 #(fdisk -l /dev/sda)=$(fdisk -l /dev/sda)
-LINPART=$(echo "$(fdisk -l /dev/sda)" | grep -e Linux | grep -v "Linux swap" | awk '{ print $1 }')
-DSIZE=$(parted /dev/sda -s -- p | grep Disk | head -n 1 | awk '{ print $3 }' | awk -F'G' '{ print $1 }'| awk -F'.' '{ print $1 }')
+linpart=$(echo "$(fdisk -l /dev/sda)" | grep -e Linux | grep -v "Linux swap" | awk '{ print $1 }')
+#dsize=$(parted /dev/sda -s -- p | grep Disk | head -n 1 | awk '{ print $3 }' | awk -F'G' '{ print $1 }'| awk -F'.' '{ print $1 }')
 
 #if there are more Linux partitions delete it all
 #NOT TESTED YET
-#if [ $(echo $LINPART | wc -l) -gt 1 ]; then
+#if [ $(echo $linpart | wc -l) -gt 1 ]; then
 #	parted /dev/sda -s -- mklabel msdos
 #fi
 
-#echo "!.. $LINPART ..!"
+#echo "!.. $linpart ..!"
 #cat fsdjaflsdj
 
-if [ -z "$LINPART" ]; then
-  	PEND=$(parted /dev/sda -s -- p | tail -n 2 | head -n 1 | awk '{ print $3 }' | awk -F. '{ print $1 }' | awk -F'G' ' { print $1 }')
-	while ((echo $PEND | grep -qv MB) && [ $PEND != "End" ]); do
-	  if [ $((DSIZE - PEND)) -lt 25 ]; then
+if [ -z "$linpart" ]; then
+  	pend=$(parted /dev/sda -s -- p | tail -n 2 | head -n 1 | awk '{ print $3 }' | awk -F. '{ print $1 }' | awk -F'G' ' { print $1 }')
+	while ((echo $pend | grep -qv MB) && [ $pend != "End" ]); do
+	  if [ $((dsize - pend)) -lt 25 ]; then
 	    PARTN=$(echo "$(fdisk -l /dev/sda)" | grep sda | grep -v Disk | wc -l)
 	    echo "Deleting partition $PARTN..."
 	    parted /dev/sda -s -- rm $PARTN
-	    PEND=$(parted /dev/sda -s -- p | tail -n 2 | head -n 1 | awk '{ print $3 }' | awk -F. '{ print $1 }' | awk -F'G' ' { print $1 }')
+	    pend=$(parted /dev/sda -s -- p | tail -n 2 | head -n 1 | awk '{ print $3 }' | awk -F. '{ print $1 }' | awk -F'G' ' { print $1 }')
 	  fi
 	done
         #elif echo "$(fdisk -l /dev/sda)" | grep -q -e FAT -e NTFS; then
-        #  PEND=$(parted /dev/sda -s -- p | tail -n 2 | head -n 1 | awk '{ print $3 }' | awk -F. '{ print $1 }' | awk -F'G' ' { print $1 }')
-        #  if [ $PEND -eq $DSIZE ]; then
-        #    LINPART=$(echo "$(fdisk -l /dev/sda)" | tail -n 1 | awk '{ print $1 }')
-        #    echo $LINPART $PEND
-        #    #yes | parted -s /dev/sda resizepart ${LINPART: -1} "$((PEND-14))GB" 
-        #    echo ", -20G" | sfdisk -N ${LINPART: -1} /dev/sda
-        #    PEND=$(parted /dev/sda -s -- p | tail -n 2 | head -n 1 | awk '{ print $3 }' | awk -F. '{ print $1 }' | awk -F'G' ' { print $1 }')
+        #  pend=$(parted /dev/sda -s -- p | tail -n 2 | head -n 1 | awk '{ print $3 }' | awk -F. '{ print $1 }' | awk -F'G' ' { print $1 }')
+        #  if [ $pend -eq $dsize ]; then
+        #    linpart=$(echo "$(fdisk -l /dev/sda)" | tail -n 1 | awk '{ print $1 }')
+        #    echo $linpart $pend
+        #    #yes | parted -s /dev/sda resizepart ${linpart: -1} "$((pend-14))GB" 
+        #    echo ", -20G" | sfdisk -N ${linpart: -1} /dev/sda
+        #    pend=$(parted /dev/sda -s -- p | tail -n 2 | head -n 1 | awk '{ print $3 }' | awk -F. '{ print $1 }' | awk -F'G' ' { print $1 }')
         #  fi
-        #  UNIT="GB"
-        #  PSIZE=$((PEND+10))
+        #  unit="GB"
+        #  psize=$((pend+10))
         #else
-        #  PEND="1"
-        #  UNIT="MB"
-        #  PSIZE=$((DSIZE-4))
+        #  pend="1"
+        #  unit="MB"
+        #  psize=$((dsize-4))
         #  parted /dev/sda -s -- mklabel msdos
 	#fi
-	PEND=$(parted /dev/sda -s -- p | tail -n 2 | head -n 1 | awk '{ print $3 }')
-	#echo $PEND
-	if [ $PEND == "End" ]; then
-		PEND=1
-		UNIT=MiB
+	pend=$(parted /dev/sda -s -- p | tail -n 2 | head -n 1 | awk '{ print $3 }')
+	#echo $pend
+	if [ $pend == "End" ]; then
+		pend=1
+		unit=MiB
 	fi
 
-	PSIZE=$((DSIZE-5))
-        parted /dev/sda -s -- mkpart extended $PEND$UNIT 100%
-	if [ $PEND == "1" ]; then
-		PEND=2
+	psize=$((dsize-5))
+        parted /dev/sda -s -- mkpart extended $pend$unit 100%
+	if [ $pend == "1" ]; then
+		pend=2
 	fi
-        parted /dev/sda -s -- mkpart logical ext4 $PEND$UNIT $PSIZE"GB"
-        parted /dev/sda -s -- mkpart logical linux-swap $PSIZE"GB" 100%
+        parted /dev/sda -s -- mkpart logical ext4 $pend$unit $psize"GB"
+        parted /dev/sda -s -- mkpart logical linux-swap $psize"GB" 100%
 fi
 
-SWAPP=$(echo "$(fdisk -l /dev/sda)" | grep "Linux swap" | awk '{ print $1 }')
-LINPART=$(echo "$(fdisk -l /dev/sda)" | grep -e Linux | grep -v "Linux swap" | awk '{ print $1 }')
+swapp=$(echo "$(fdisk -l /dev/sda)" | grep "Linux swap" | awk '{ print $1 }')
+linpart=$(echo "$(fdisk -l /dev/sda)" | grep -e Linux | grep -v "Linux swap" | awk '{ print $1 }')
 
-yes | mkfs.ext4 $LINPART
+yes | mkfs.ext4 $linpart
 #starts even if it makes no sense
-#mkfs.ext4 -F $LINPART
+#mkfs.ext4 -F $linpart
 
-if [ -n "$(swapon -s)" ]; then swapoff $SWAPP; fi
-if [ -z "$SWAPP" ]; then
-	yes | mkswap $SWAPP
-	swapon $SWAPP
+if [ -n "$(swapon -s)" ]; then swapoff $swapp; fi
+if [ -z "$swapp" ]; then
+	yes | mkswap $swapp
+	swapon $swapp
 fi
 
-mount $LINPART /mnt
+mount $linpart /mnt
 
 apt-get update
 apt-get -yq install debootstrap
@@ -99,7 +101,7 @@ cp rc.local /mnt/etc/
 chmod +x chroot.sh	
 cp chroot.sh /mnt/chroot.sh 
 cp scratch2-install.sh /mnt/scratch2-install.sh 
-chroot /mnt ./chroot.sh $LINPART $SWAPP $HOSTNAME $PASSWD
+chroot /mnt ./chroot.sh $linpart $swapp $hostname $passwd
 
 read -p "Press Enter to reboot"
 
